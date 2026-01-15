58ba42c4efaa467df781c60d43cfac07c79afff4
jonghyun <jhs.on.lab@gmail.com>
2026-01-16 01:28:45 +0900

    first commit

---
+++ src/main/java/me/son/excelkit/ExcelKitApplication.java
@@ -0,0 +1,13 @@
+package me.son.excelkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/son/excelkit/ExcelKitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.son.excelkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
